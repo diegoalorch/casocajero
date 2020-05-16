ff48217386e917d0ce27ed7093fecaf1b4b578f0
diegoalor <diegoalor@upeu.edu.pe>
2020-05-15 23:42:24 -0500

    asw

--- js/script.js
+++ js/script.js
@@ -1,34 +1,58 @@
-var con = 0
 var pass=""
 var clave_real= "12345678"
-function cambiar(key) {
-  switch (key) {
+var siguiente = false
+// Display = 'none'(sirve para que no se vea) 
+// Display = "block"(sirve para que se vea) 
+function cambiar(opcion) {
+  switch (opcion) {
     case 1:
         if (pass.length==8) {
             if (pass==clave_real) {
                 document.getElementById('MostrarOcultar').style.display='none';
                 document.getElementById('vistaMenuPrincipal').style.display="block";
+                siguiente = true
             }else{
                 alert("clave incorrecta")
                 document.getElementById("pass").value=""
                 pass=""
             }
         } else {
-            alert("ingrese la clave")
-            document.getElementById("pass").value=""
-            pass=""
+            if (pass.length < 8 && pass.length!=0) {
+                alert("Clave Incompleta")
+                document.getElementById("pass").value=""
+                pass=""
+            }else{
+                alert("Ingrese Clave")
+                document.getElementById("pass").value=""
+                pass=""
+            }
         }
-      
         break;
     case 2:
       document.getElementById('MostrarOcultar').style.display='block';
-      document.getElementById('vistaMenuPrincipal').style.display="none";     
+      document.getElementById('vistaMenuPrincipal').style.display="none";
+      document.getElementById("pass").value=""
+      pass=""
      // con++
         break;
     default:
         alert(key)
         console.log(key)
         break;
+    case 3:
+        //substring (divide un string) y length (cuenta las vocales)
+        var x = document.getElementById("pass").value.length
+        var y = pass.length
+        document.getElementById("pass").value=document.getElementById("pass").value.substring(0,x-1)
+        pass=pass.substring(0,y-1)
+        console.log(pass)
+        break;
+    case 4:
+        if (siguiente) {
+            alert("Soy un Boton de pantalla")
+        }else{
+            alert("Los botones aun no esta desbloqueados")
+        }
   }
 }
 function password(numero) {
